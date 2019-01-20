0e3da5a0609c224abb3c11090002a52a68975975
Dennis Kieselhorst <mail@dekies.de>
2019-01-20 13:58:05 +0100

    move ClassElement and MethodElement to upper level to reuse them in non-cdi implementation

--- geronimo-openapi-maven-plugin/src/main/java/org/apache/geronimo/microprofile/openapi/mojo/OpenAPIMojo.java
+++ geronimo-openapi-maven-plugin/src/main/java/org/apache/geronimo/microprofile/openapi/mojo/OpenAPIMojo.java
@@ -16,11 +16,7 @@
  */
 package org.apache.geronimo.microprofile.openapi.mojo;
 
-import static java.util.Arrays.asList;
 import static java.util.Optional.ofNullable;
-import static java.util.function.Function.identity;
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toMap;
 import static org.apache.maven.plugins.annotations.LifecyclePhase.PROCESS_CLASSES;
 import static org.apache.maven.plugins.annotations.ResolutionScope.COMPILE_PLUS_RUNTIME;
 
@@ -28,19 +24,13 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
-import java.lang.annotation.Annotation;
-import java.lang.reflect.AnnotatedElement;
-import java.lang.reflect.Method;
-import java.lang.reflect.Type;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import javax.json.bind.Jsonb;
@@ -49,9 +39,9 @@ import javax.json.bind.JsonbConfig;
 
 import org.apache.geronimo.microprofile.openapi.impl.model.InfoImpl;
 import org.apache.geronimo.microprofile.openapi.impl.model.OpenAPIImpl;
-import org.apache.geronimo.microprofile.openapi.impl.processor.AnnotatedMethodElement;
-import org.apache.geronimo.microprofile.openapi.impl.processor.AnnotatedTypeElement;
 import org.apache.geronimo.microprofile.openapi.impl.processor.AnnotationProcessor;
+import org.apache.geronimo.microprofile.openapi.impl.processor.reflect.ClassElement;
+import org.apache.geronimo.microprofile.openapi.impl.processor.reflect.MethodElement;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -178,151 +168,4 @@ public class OpenAPIMojo extends AbstractMojo {
         }
     }
 
-    private static Annotation[] mergeAnnotations(final AnnotatedElement... element) {
-        return Stream.of(element)
-                     .flatMap(i -> Stream.of(i.getAnnotations()))
-                     .collect(toMap(Annotation::annotationType, identity(), (a, b) -> a))
-                     .values()
-                     .toArray(new Annotation[0]);
-    }
-
-    private static class MethodElement implements AnnotatedMethodElement {
-        private static final Method[] NO_METHOD = new Method[0];
-
-        private final Method[] delegates;
-        private Annotation[] annotations;
-
-        private MethodElement(final Method method) {
-            final Collection<Method> methods = new LinkedList<>();
-            methods.add(method);
-            Stream.of(method.getDeclaringClass().getInterfaces())
-                  .map(it -> {
-                      try {
-                          return it.getMethod(method.getName(), method.getParameterTypes());
-                      } catch (final NoSuchMethodException e) {
-                          return null;
-                      }
-                  })
-                  .filter(Objects::nonNull).forEach(methods::add);
-            this.delegates = methods.toArray(NO_METHOD);
-        }
-
-        @Override
-        public String getName() {
-            return delegates[0].getName();
-        }
-
-        @Override
-        public Type getReturnType() {
-            return delegates[0].getGenericReturnType();
-        }
-
-        @Override
-        public Class<?> getDeclaringClass() {
-            return delegates[0].getDeclaringClass();
-        }
-
-        @Override
-        public AnnotatedTypeElement[] getParameters() {
-            final java.lang.reflect.Parameter[] parameters = delegates[0].getParameters();
-            return IntStream.range(0, parameters.length)
-                        .mapToObj(p -> new AnnotatedTypeElement() {
-                            private Annotation[] annotations;
-
-                            @Override
-                            public Type getType() {
-                                return parameters[p].getParameterizedType();
-                            }
-
-                            @Override // todo:
-                            public <T extends Annotation> T getAnnotation(final Class<T> annotationClass) {
-                                return Stream.of(delegates)
-                                        .map(m -> m.getParameters()[p])
-                                        .filter(it -> it.isAnnotationPresent(annotationClass))
-                                        .map(it -> it.getAnnotation(annotationClass))
-                                        .findFirst()
-                                        .orElse(null);
-                            }
-
-                            @Override
-                            public Annotation[] getAnnotations() {
-                                return annotations == null ? annotations = mergeAnnotations(delegates) : annotations;
-                            }
-
-                            @Override
-                            public Annotation[] getDeclaredAnnotations() {
-                                return getAnnotations();
-                            }
-                        }).toArray(AnnotatedTypeElement[]::new);
-        }
-
-        @Override
-        public <T extends Annotation> T getAnnotation(final Class<T> annotationClass) {
-            return Stream.of(delegates)
-                         .filter(d -> d.isAnnotationPresent(annotationClass))
-                         .map(d -> d.getAnnotation(annotationClass))
-                         .findFirst().orElse(null);
-        }
-
-        @Override
-        public Annotation[] getAnnotations() {
-            return annotations == null ? annotations = mergeAnnotations(delegates) : annotations;
-        }
-
-        @Override
-        public Annotation[] getDeclaredAnnotations() {
-            return getAnnotations();
-        }
-    }
-
-    private static class ClassElement implements AnnotatedTypeElement {
-        private final Class<?> delegate;
-        private Annotation[] annotations;
-
-        private ClassElement(final Class<?> delegate) {
-            this.delegate = delegate;
-        }
-
-        @Override
-        public <T extends Annotation> T getAnnotation(final Class<T> annotationClass) {
-            return ofNullable(delegate.getAnnotation(annotationClass))
-                    .orElseGet(() -> findInInterfaces(annotationClass));
-        }
-
-        @Override
-        public Annotation[] getAnnotations() {
-            return annotations == null ? annotations = gatherAnnotations() : annotations;
-        }
-
-        @Override
-        public Annotation[] getDeclaredAnnotations() {
-            return getAnnotations();
-        }
-
-        @Override
-        public Type getType() {
-            return delegate;
-        }
-
-        private Annotation[] gatherAnnotations() {
-            final Collection<Annotation> annotations = new LinkedList<>(asList(delegate.getAnnotations()));
-            annotations.addAll(findInterfaces(delegate)
-                    .flatMap(i -> Stream.of(i.getAnnotations()).filter(it -> !delegate.isAnnotationPresent(it.annotationType())))
-                    .distinct()
-                    .collect(toList()));
-            return annotations.toArray(new Annotation[0]);
-        }
-
-        private <T extends Annotation> T findInInterfaces(final Class<T> annotationClass) {
-            return findInterfaces(delegate)
-                    .filter(it -> it.isAnnotationPresent(annotationClass))
-                    .findFirst()
-                    .map(it -> it.getAnnotation(annotationClass))
-                    .orElse(null);
-        }
-
-        private Stream<Class<?>> findInterfaces(final Class<?> delegate) {
-            return Stream.of(delegate.getInterfaces());
-        }
-    }
 }
